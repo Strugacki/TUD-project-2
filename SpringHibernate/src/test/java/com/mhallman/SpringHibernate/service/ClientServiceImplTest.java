9b814b579634bc6a16e7e736f23b66963261f8b6
Strugacki <mhallman94@gmail.com>
2016-01-27 12:00:24 +0100

    Project completed

--- SpringHibernate/src/test/java/com/mhallman/SpringHibernate/service/ClientServiceImplTest.java
+++ SpringHibernate/src/test/java/com/mhallman/SpringHibernate/service/ClientServiceImplTest.java
@@ -1,9 +1,11 @@
 package com.mhallman.SpringHibernate.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.After;
@@ -36,17 +38,24 @@ public class ClientServiceImplTest {
 	private final String lastName1 = "Urban";
 	private final String phoneNumber1 ="123321123";
 	
+	private List<Client> clientsToTest;
+	
 	@Before
 	public void fill(){
+		clientsToTest = new ArrayList<Client>();
 		Client client = new Client(firstName,lastName,phoneNumber);
 		cs.addClient(client);
+		clientsToTest.add(client);
 		Client client1 = new Client(firstName1,lastName1,phoneNumber1);
 		cs.addClient(client1);
+		clientsToTest.add(client1);
 	}
 	
 	@After
 	public void clean(){
-		List<Client> clients = cs.getAllClients();
+		List<Client> clients = new ArrayList<Client>();
+		clients.add(cs.getClientById(clientsToTest.get(0).getId()));
+		clients.add(cs.getClientById(clientsToTest.get(1).getId()));
 		for(Client client : clients){
 			cs.deleteClient(client);
 		}
@@ -55,7 +64,9 @@ public class ClientServiceImplTest {
 	
 	@Test
 	public void checkAdding(){
-		List<Client> clients = cs.getAllClients();
+		List<Client> clients = new ArrayList<Client>();
+		clients.add(cs.getClientById(clientsToTest.get(0).getId()));
+		clients.add(cs.getClientById(clientsToTest.get(1).getId()));
 		Client toCheck = cs.getClientById(clients.get(0).getId());
 		
 		assertEquals(firstName,toCheck.getFirstName());
@@ -66,33 +77,38 @@ public class ClientServiceImplTest {
 	@Test
 	public void checkDeleting(){
 		
-		List<Client> clients = cs.getAllClients();
-		Client toCheck = cs.getClientById(clients.get(0).getId());
-		int size = cs.getAllClients().size();
+		Client toDelete = new Client(firstName1,lastName1,phoneNumber1);
+		cs.addClient(toDelete);
 		
-		assertEquals(firstName,toCheck.getFirstName());
-		assertEquals(lastName,toCheck.getLastName());
-		assertEquals(phoneNumber,toCheck.getPhoneNumber());
-		assertEquals(size,2);
+		Client toCheck = cs.getClientById(toDelete.getId());
+		
+		int size = cs.getAllClients().size();
+		assertEquals(firstName1,toCheck.getFirstName());
+		assertEquals(lastName1,toCheck.getLastName());
 		
 		cs.deleteClient(toCheck);
-		size = cs.getAllClients().size();
-		assertEquals(size,1);
+		
+		int afterDeleteSize = cs.getAllClients().size();
+		assertNotSame(size,afterDeleteSize);
 		assertNull(cs.getClientById(toCheck.getId()));
 	}
 	
 	@Test
 	public void checkGettingAll(){
-		List<Client> clients = cs.getAllClients();
-		int size = clients.size();
-		assertEquals(size,2);
+		List<Client> clients = new ArrayList<Client>();
+		clients.add(cs.getClientById(clientsToTest.get(0).getId()));
+		clients.add(cs.getClientById(clientsToTest.get(1).getId()));
+		int size = cs.getAllClients().size();
+		assertNotNull(size);
 		assertEquals(firstName,clients.get(0).getFirstName());
 		assertEquals(firstName1,clients.get(1).getFirstName());
 	}
 	
 	@Test
 	public void checkUpdating(){
-		List<Client> clients = cs.getAllClients();
+		List<Client> clients = new ArrayList<Client>();
+		clients.add(cs.getClientById(clientsToTest.get(0).getId()));
+		clients.add(cs.getClientById(clientsToTest.get(1).getId()));
 		Client toCheck = cs.getClientById(clients.get(0).getId());
 		
 		assertEquals(firstName,toCheck.getFirstName());
